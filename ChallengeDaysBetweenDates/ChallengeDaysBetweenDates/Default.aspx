dc279dbdb75917c4ed271e8f49907dd0920fb488
Alex <brownalex275@gmail.com>
2017-08-09 12:03:21 -0400

    Finished ChallengeDaysBetweenDates

---
+++ ChallengeDaysBetweenDates/ChallengeDaysBetweenDates/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeDaysBetweenDates.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            How many days have elapsed?<br />
+            <br />
+            Choose one date:<asp:Calendar ID="firstCalendar" runat="server"></asp:Calendar>
+            <br />
+            <br />
+            Choose a second date:<asp:Calendar ID="secondCalendar" runat="server"></asp:Calendar>
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="OK" Width="34px" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeDaysBetweenDates/ChallengeDaysBetweenDates/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeDaysBetweenDates
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DateTime first = firstCalendar.SelectedDate;
+            DateTime second = secondCalendar.SelectedDate;
+            TimeSpan myTimeSpan = TimeSpan.Parse("3.4:10:50.32");
+            if (first > second)
+            {
+                myTimeSpan = first.Subtract(second);
+                resultLabel.Text = myTimeSpan.TotalDays.ToString();
+            }
+            else if (second > first)
+            {
+                myTimeSpan = second.Subtract(first);
+                resultLabel.Text = myTimeSpan.TotalDays.ToString();
+            }
+            else
+            {
+                myTimeSpan = first.Subtract(second);
+                resultLabel.Text = myTimeSpan.TotalDays.ToString();
+            }
+            resultLabel.Text = myTimeSpan.TotalDays.ToString();
+
+        }
+    }
+}
