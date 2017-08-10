941186ea56a4268c08d65bbbd8739c497abfceda
Alex <brownalex275@gmail.com>
2017-08-10 16:01:10 -0400

    Finished ChallengeEpicSpiesAssetTracker

---
+++ ChallengeEpicSpiesAssetTracker/ChallengeEpicSpiesAssetTracker/Default.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeEpicSpiesAssetTracker.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 150px;
+            height: 150px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="font-family: Arial, Helvetica, sans-serif">
+            <h2>
+                <img class="auto-style1" src="epic-spies-logo.jpg" /><br />
+                <br />
+                Asset Performance Tracker</h2>
+            Asset Name:&nbsp;
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <p>
+                Elections Rigged:
+                <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                Acts of Subterfuge Performed: <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Add Asset" Width="93px" />
+            </p>
+            <p>
+                <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            </p>
+            <p>
+                &nbsp;</p>
+        </div>
+    </form>
+    <p>
+        &nbsp;</p>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssetTracker/ChallengeEpicSpiesAssetTracker/Default.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssetTracker
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                string[] names = new string[0];
+                int[] elections = new int[0];
+                int[] acts = new int[0];
+                ViewState.Add("names", names);
+                ViewState.Add("elections", elections);
+                ViewState.Add("acts", acts);
+
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string[] names = (string[])ViewState["names"];
+            int[] elections = (int[])ViewState["elections"];
+            int[] acts = (int[])ViewState["acts"];
+
+            Array.Resize(ref names, names.Length + 1);
+            Array.Resize(ref elections, elections.Length + 1);
+            Array.Resize(ref acts, acts.Length + 1);
+
+            int newestItem = names.GetUpperBound(0);
+
+            names[newestItem] = TextBox1.Text;
+            elections[newestItem] = int.Parse(TextBox2.Text);
+            acts[newestItem] = int.Parse(TextBox3.Text);
+
+            ViewState["names"] = names;
+            ViewState["elections"] = elections;
+            ViewState["acts"] = acts;
+
+            resultLabel.Text = String.Format("Total Elections Rigged: {0} <br /> Average Acts of Subterfuge per Asset: {1:N2} <br />" +
+                " (Last Asset you Added: {2})", elections.Sum(), acts.Average(), names[newestItem]);
+
+        }
+    }
+}
