e1d16990874a38d4dcc4c58917564001033e2f36
Alex <brownalex275@gmail.com>
2017-10-07 20:59:21 -0400

    Completed ComicBookChallenge

---
+++ FirstChallenge/FirstChallenge/Controllers/ComicController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstChallenge.Controllers
+{
+    public class ComicController : Controller
+    {
+        // GET: Comic
+        public ActionResult Index()
+        {
+            var comics = Models.ComicBookManager.GetComicBooks();
+            return View(comics);
+        }
+        // GET: Products/Details/5
+        public ActionResult Detail(int id)
+        {
+            var comics = Models.ComicBookManager.GetComicBooks();
+            var comic = comics.Find(p => p.ComicBookId == id);
+
+
+            return View(comic);
+        }
+    }
+   
+}
---
+++ PapaBobs/dbo.Order.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Order] (
+    [orderId]   UNIQUEIDENTIFIER NOT NULL,
+    [size]      VARCHAR (50)     NOT NULL,
+    [crust]     VARCHAR (50)     NOT NULL,
+    [toppings]  VARCHAR (50)     NOT NULL,
+    [name]      VARCHAR (50)     NOT NULL,
+    [address]   VARCHAR (50)     NOT NULL,
+    [zip]       VARCHAR (50)     NOT NULL,
+    [phone]     VARCHAR (50)     NOT NULL,
+    [payment]   VARCHAR (50)     NOT NULL,
+    [completed] BIT              DEFAULT ((0)) NOT NULL,
+    PRIMARY KEY CLUSTERED ([orderId] ASC)
+
+	
+);
+
+
